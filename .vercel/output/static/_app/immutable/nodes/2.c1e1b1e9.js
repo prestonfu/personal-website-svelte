ecdf14a3e310ac267e5a25a946bcc13a22330b37
prestonfu <prestonfu@berkeley.edu>
2024-03-01 23:13:05 -0800

    attempt 3

--- .vercel/output/static/_app/immutable/nodes/2.c1e1b1e9.js
+++ .vercel/output/static/_app/immutable/nodes/2.c1e1b1e9.js
@@ -1,17 +1,202 @@
-import{S as N,i as O,s as Q,y as W,a as z,k as o,q as l,z as X,c as G,l as d,m as p,r as s,h as i,n as t,A as Y,b as H,D as e,F as Z,g as ee,d as te,B as ae}from"../chunks/index.4b79d54d.js";import{S as re}from"../chunks/Seo.c895259e.js";function ne(J){let c,k,_,u,a,x,h,E,B,m,$,D,y,P,S,f,C,L,v,U,j,q,b,F,A;return c=new re({props:{title:"Preston Fu",description:"Undergrad at UC Berkeley"}}),{c(){W(c.$$.fragment),k=z(),_=o("div"),u=o("div"),a=o("p"),x=l(`I'm a second-year electrical engineering and computer science major UC
+import {
+  S as N,
+  i as O,
+  s as Q,
+  y as W,
+  a as z,
+  k as o,
+  q as l,
+  z as X,
+  c as G,
+  l as d,
+  m as p,
+  r as s,
+  h as i,
+  n as t,
+  A as Y,
+  b as H,
+  D as e,
+  F as Z,
+  g as ee,
+  d as te,
+  B as ae,
+} from "../chunks/index.4b79d54d.js";
+import { S as re } from "../chunks/Seo.c895259e.js";
+function ne(J) {
+  let c, k, _, u, a, x, h, E, B, m, $, D, y, P, S, f, C, L, v, U, j, q, b, F, A;
+  return (
+    (c = new re({
+      props: { title: "Preston Fu", description: "Undergrad at UC Berkeley" },
+    })),
+    {
+      c() {
+        W(c.$$.fragment),
+          (k = z()),
+          (_ = o("div")),
+          (u = o("div")),
+          (a = o("p")),
+          (x =
+            l(`I'm a second-year electrical engineering and computer science major UC
       Berkeley. I am a researcher in the
-      `),h=o("a"),E=l("Berkeley Artificial Intelligence Research Lab"),B=l(`
+      `)),
+          (h = o("a")),
+          (E = l("Berkeley Artificial Intelligence Research Lab")),
+          (B = l(`
       and am advised by
-      `),m=o("a"),$=l("Sergey Levine"),D=l(`. Previously, I have been advised by
-      `),y=o("a"),P=l("Dan Klein"),S=l(`,
-      `),f=o("a"),C=l("Trevor Darrell"),L=l(`, and
-      `),v=o("a"),U=l("Alexandre Bayen"),j=l("."),q=z(),b=o("p"),F=l(`My technical interests include reinforcement learning, computer vision,
-      probability, and algorithms.`),this.h()},l(r){X(c.$$.fragment,r),k=G(r),_=d(r,"DIV",{class:!0});var g=p(_);u=d(g,"DIV",{class:!0});var I=p(u);a=d(I,"P",{});var n=p(a);x=s(n,`I'm a second-year electrical engineering and computer science major UC
+      `)),
+          (m = o("a")),
+          ($ = l("Sergey Levine")),
+          (D = l(`. Previously, I have been advised by
+      `)),
+          (y = o("a")),
+          (P = l("Dan Klein")),
+          (S = l(`,
+      `)),
+          (f = o("a")),
+          (C = l("Trevor Darrell")),
+          (L = l(`, and
+      `)),
+          (v = o("a")),
+          (U = l("Alexandre Bayen")),
+          (j = l(".")),
+          (q = z()),
+          (b = o("p")),
+          (F =
+            l(`My technical interests include reinforcement learning, computer vision,
+      probability, and algorithms.`)),
+          this.h();
+      },
+      l(r) {
+        X(c.$$.fragment, r), (k = G(r)), (_ = d(r, "DIV", { class: !0 }));
+        var g = p(_);
+        u = d(g, "DIV", { class: !0 });
+        var I = p(u);
+        a = d(I, "P", {});
+        var n = p(a);
+        (x = s(
+          n,
+          `I'm a second-year electrical engineering and computer science major UC
       Berkeley. I am a researcher in the
-      `),h=d(n,"A",{class:!0,href:!0,target:!0});var K=p(h);E=s(K,"Berkeley Artificial Intelligence Research Lab"),K.forEach(i),B=s(n,`
+      `
+        )),
+          (h = d(n, "A", { class: !0, href: !0, target: !0 }));
+        var K = p(h);
+        (E = s(K, "Berkeley Artificial Intelligence Research Lab")),
+          K.forEach(i),
+          (B = s(
+            n,
+            `
       and am advised by
-      `),m=d(n,"A",{class:!0,href:!0,target:!0});var M=p(m);$=s(M,"Sergey Levine"),M.forEach(i),D=s(n,`. Previously, I have been advised by
-      `),y=d(n,"A",{class:!0,href:!0,target:!0});var R=p(y);P=s(R,"Dan Klein"),R.forEach(i),S=s(n,`,
-      `),f=d(n,"A",{class:!0,href:!0,target:!0});var T=p(f);C=s(T,"Trevor Darrell"),T.forEach(i),L=s(n,`, and
-      `),v=d(n,"A",{class:!0,href:!0,target:!0});var V=p(v);U=s(V,"Alexandre Bayen"),V.forEach(i),j=s(n,"."),n.forEach(i),q=G(I),b=d(I,"P",{});var w=p(b);F=s(w,`My technical interests include reinforcement learning, computer vision,
-      probability, and algorithms.`),w.forEach(i),I.forEach(i),g.forEach(i),this.h()},h(){t(h,"class","link"),t(h,"href","https://bair.berkeley.edu/"),t(h,"target","_blank"),t(m,"class","link"),t(m,"href","https://people.eecs.berkeley.edu/~svlevine/"),t(m,"target","_blank"),t(y,"class","link"),t(y,"href","https://people.eecs.berkeley.edu/~klein/"),t(y,"target","_blank"),t(f,"class","link"),t(f,"href","https://people.eecs.berkeley.edu/~trevor/"),t(f,"target","_blank"),t(v,"class","link"),t(v,"href","https://bayen.berkeley.edu/alex-bayen"),t(v,"target","_blank"),t(u,"class","space-y-5"),t(_,"class","layout-md text-lg space-y-12")},m(r,g){Y(c,r,g),H(r,k,g),H(r,_,g),e(_,u),e(u,a),e(a,x),e(a,h),e(h,E),e(a,B),e(a,m),e(m,$),e(a,D),e(a,y),e(y,P),e(a,S),e(a,f),e(f,C),e(a,L),e(a,v),e(v,U),e(a,j),e(u,q),e(u,b),e(b,F),A=!0},p:Z,i(r){A||(ee(c.$$.fragment,r),A=!0)},o(r){te(c.$$.fragment,r),A=!1},d(r){ae(c,r),r&&i(k),r&&i(_)}}}class ie extends N{constructor(c){super(),O(this,c,null,ne,Q,{})}}export{ie as component};
+      `
+          )),
+          (m = d(n, "A", { class: !0, href: !0, target: !0 }));
+        var M = p(m);
+        ($ = s(M, "Sergey Levine")),
+          M.forEach(i),
+          (D = s(
+            n,
+            `. Previously, I have been advised by
+      `
+          )),
+          (y = d(n, "A", { class: !0, href: !0, target: !0 }));
+        var R = p(y);
+        (P = s(R, "Dan Klein")),
+          R.forEach(i),
+          (S = s(
+            n,
+            `,
+      `
+          )),
+          (f = d(n, "A", { class: !0, href: !0, target: !0 }));
+        var T = p(f);
+        (C = s(T, "Trevor Darrell")),
+          T.forEach(i),
+          (L = s(
+            n,
+            `, and
+      `
+          )),
+          (v = d(n, "A", { class: !0, href: !0, target: !0 }));
+        var V = p(v);
+        (U = s(V, "Alexandre Bayen")),
+          V.forEach(i),
+          (j = s(n, ".")),
+          n.forEach(i),
+          (q = G(I)),
+          (b = d(I, "P", {}));
+        var w = p(b);
+        (F = s(
+          w,
+          `My technical interests include reinforcement learning, computer vision,
+      probability, and algorithms.`
+        )),
+          w.forEach(i),
+          I.forEach(i),
+          g.forEach(i),
+          this.h();
+      },
+      h() {
+        t(h, "class", "link"),
+          t(h, "href", "https://bair.berkeley.edu/"),
+          t(h, "target", "_blank"),
+          t(m, "class", "link"),
+          t(m, "href", "https://people.eecs.berkeley.edu/~svlevine/"),
+          t(m, "target", "_blank"),
+          t(y, "class", "link"),
+          t(y, "href", "https://people.eecs.berkeley.edu/~klein/"),
+          t(y, "target", "_blank"),
+          t(f, "class", "link"),
+          t(f, "href", "https://people.eecs.berkeley.edu/~trevor/"),
+          t(f, "target", "_blank"),
+          t(v, "class", "link"),
+          t(v, "href", "https://bayen.berkeley.edu/alex-bayen"),
+          t(v, "target", "_blank"),
+          t(u, "class", "space-y-5"),
+          t(_, "class", "layout-md text-lg space-y-12");
+      },
+      m(r, g) {
+        Y(c, r, g),
+          H(r, k, g),
+          H(r, _, g),
+          e(_, u),
+          e(u, a),
+          e(a, x),
+          e(a, h),
+          e(h, E),
+          e(a, B),
+          e(a, m),
+          e(m, $),
+          e(a, D),
+          e(a, y),
+          e(y, P),
+          e(a, S),
+          e(a, f),
+          e(f, C),
+          e(a, L),
+          e(a, v),
+          e(v, U),
+          e(a, j),
+          e(u, q),
+          e(u, b),
+          e(b, F),
+          (A = !0);
+      },
+      p: Z,
+      i(r) {
+        A || (ee(c.$$.fragment, r), (A = !0));
+      },
+      o(r) {
+        te(c.$$.fragment, r), (A = !1);
+      },
+      d(r) {
+        ae(c, r), r && i(k), r && i(_);
+      },
+    }
+  );
+}
+class ie extends N {
+  constructor(c) {
+    super(), O(this, c, null, ne, Q, {});
+  }
+}
+export { ie as component };
